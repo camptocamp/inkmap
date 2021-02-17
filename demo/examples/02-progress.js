aa7c3baea159c351750ff5f4bdb3310d81deee97
Laure-Hélène Bruneton <laure-helene.bruneton@camptocamp.com>
2021-02-17 09:29:28 +0100

    feat(demo): split cancel from progress example

--- demo/examples/02-progress.js
+++ demo/examples/02-progress.js
@@ -1,11 +1,9 @@
-import { cancelJob, downloadBlob, getJobStatus, queuePrint } from 'inkmap';
+import { downloadBlob, getJobStatus, queuePrint } from 'inkmap';
 
 const root = document.getElementById('example-02');
 const btn = /** @type {CustomButton} */ root.querySelector('custom-button');
 const bar = /** @type {CustomProgress} */ root.querySelector('custom-progress');
 const spec = /** @type {PrintSpec} */ root.querySelector('print-spec');
-
-let jobId;
 
 btn.addEventListener('click', async () => {
   // display the loading spinner
@@ -16,30 +14,21 @@ btn.addEventListener('click', async () => {
   bar.status = 'pending';
 
   // create a job, get a promise that resolves with the job id
-  jobId = await queuePrint(spec.value);
+  const jobId = await queuePrint(spec.value);
 
   getJobStatus(jobId).subscribe((printStatus) => {
     // update the job progress
     bar.progress = printStatus.progress;
     bar.status = printStatus.status;
 
-    // job is finished or canceled
-    if (printStatus.progress === 1 || printStatus.progress === -1) {
-      // hide the loading spinner
-      btn.working = false;
-    }
-
     // job is finished
     if (printStatus.progress === 1) {
+      // hide the loading spinner
+      btn.working = false;
+
       // download the result
       const filename = `inkmap-${new Date().toISOString().substr(0, 10)}.png`;
       downloadBlob(printStatus.imageBlob, filename);
     }
   });
-});
-
-const btnCancel = /** @type {Button} */ root.querySelector('.cancel-btn');
-
-btnCancel.addEventListener('click', async () => {
-  cancelJob(jobId);
 });
---
+++ demo/examples/03-cancel.js
@@ -0,0 +1,45 @@
+import { cancelJob, downloadBlob, getJobStatus, queuePrint } from 'inkmap';
+
+const root = document.getElementById('example-03');
+const btn = /** @type {CustomButton} */ root.querySelector('custom-button');
+const bar = /** @type {CustomProgress} */ root.querySelector('custom-progress');
+const spec = /** @type {PrintSpec} */ root.querySelector('print-spec');
+
+let jobId;
+
+btn.addEventListener('click', async () => {
+  // display the loading spinner
+  btn.working = true;
+
+  // display the job progress
+  bar.progress = 0;
+  bar.status = 'pending';
+
+  // create a job, get a promise that resolves with the job id
+  jobId = await queuePrint(spec.value);
+
+  getJobStatus(jobId).subscribe((printStatus) => {
+    // update the job progress
+    bar.progress = printStatus.progress;
+    bar.status = printStatus.status;
+
+    // job is finished or canceled
+    if (printStatus.progress === 1 || printStatus.progress === -1) {
+      // hide the loading spinner
+      btn.working = false;
+    }
+
+    // job is finished
+    if (printStatus.progress === 1) {
+      // download the result
+      const filename = `inkmap-${new Date().toISOString().substr(0, 10)}.png`;
+      downloadBlob(printStatus.imageBlob, filename);
+    }
+  });
+});
+
+const btnCancel = /** @type {Button} */ root.querySelector('.cancel-btn');
+
+btnCancel.addEventListener('click', async () => {
+  cancelJob(jobId);
+});
