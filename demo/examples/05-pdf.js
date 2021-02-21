4b0d04bba2e650b9ae5326179c6137fceabbe766
Olivier Guyot <olivier.guyot@camptocamp.com>
2021-02-21 15:32:28 +0100

    feat(demo): greatly simplify pdf example
    Use CreateObjectURL instead of a file reader, and drop the flexible
    PDF spec to just add a title and creation date alongside
    the map.

--- demo/examples/05-pdf.js
+++ demo/examples/05-pdf.js
@@ -11,10 +11,13 @@ spec.onValidityCheck((valid) => (btn.enabled = valid));
 btn.addEventListener('click', async () => {
   btn.showSpinner();
 
-  // Get map size from printSpec
+  const mapWidth = 277; // mm
+  const mapHeight = 150; // mm
+
+  // Force map size to fit the PDF document
   const specValue = {
     ...spec.value,
-    size: [pdfSpec.map.width, pdfSpec.map.height],
+    size: [mapWidth, mapHeight, 'mm'],
   };
 
   // create a job, get a promise that resolves when the job is finished
@@ -22,150 +25,47 @@ btn.addEventListener('click', async () => {
 
   btn.hideSpinner();
 
-  // convert the result into a Data URL
-  const reader = new FileReader();
-  reader.readAsDataURL(blob);
-  reader.onloadend = function () {
-    const imgData = reader.result;
+  // initializes the PDF document
+  const doc = new jsPDF({
+    orientation: 'landscape',
+    unit: 'mm',
+    format: 'a4',
+    putOnlyUsedFonts: true,
+  });
 
-    // add the image to a newly created PDF
-    const doc = new jsPDF({
-      orientation: pdfSpec.orientation,
-      unit: 'px',
-      format: pdfSpec.format,
-      putOnlyUsedFonts: true,
-      hotfixes: ['px_scaling'],
-    });
+  // create an Object URL from the map image blob and add it to the PDF
+  const imgUrl = URL.createObjectURL(blob);
+  doc.addImage(imgUrl, 'JPEG', 10, 40, mapWidth, mapHeight);
 
-    doc.addImage(
-      imgData,
-      'JPEG',
-      pdfSpec.map.x,
-      pdfSpec.map.y,
-      pdfSpec.map.width,
-      pdfSpec.map.height
-    );
+  // add a title
+  doc.setFont('times', 'bold');
+  doc.setFontSize(20);
+  doc.text('A fantastic map.', 148.5, 20, null, null, 'center');
 
-    // add other blocks from the pdf spec
-    for (const block of pdfSpec.blocks) {
-      if (block.rectangle) {
-        doc.rect(
-          block.rectangle.x,
-          block.rectangle.y,
-          block.rectangle.width,
-          block.rectangle.height
-        );
-      }
-      if (block.text) {
-        if (block.text.font) {
-          doc.setFont(block.text.font.name, block.text.font.style);
-          doc.setFontSize(block.text.font.size);
-        }
-        doc.text(
-          block.text.content,
-          block.text.x,
-          block.text.y,
-          null,
-          null,
-          block.text.align
-        );
-        if (pdfSpec.font) {
-          doc.setFont(pdfSpec.font.name, pdfSpec.font.style);
-          doc.setFontSize(pdfSpec.font.size);
-        }
-      }
-    }
+  // add a creation date
+  doc.setFont('courier', 'normal');
+  doc.setFontSize(12);
+  doc.text(
+    `Print date : ${new Date().toLocaleString()}`,
+    10,
+    200,
+    null,
+    null,
+    'left'
+  );
 
-    // download the result
-    doc.save('inkmap.pdf');
-  };
+  // add attribution
+  doc.setFont('courier', 'normal');
+  doc.setFontSize(12);
+  doc.text(
+    `Credit to whoever helped make this wonderful map.`,
+    287,
+    200,
+    null,
+    null,
+    'right'
+  );
+
+  // download the result
+  doc.save('inkmap.pdf');
 });
-
-const pdfSpec = {
-  // A4 landscape 96 DPI => 1123px x 794px
-  orientation: 'landscape',
-  format: 'a4',
-  font: {
-    name: 'courier',
-    style: 'normal',
-    size: 10,
-  },
-  map: {
-    x: 20,
-    y: 100,
-    width: 800,
-    height: 500,
-  },
-  blocks: [
-    {
-      id: 'title',
-      rectangle: {
-        x: 20,
-        y: 20,
-        width: 800,
-        height: 60,
-      },
-      text: {
-        content: 'Some nice title above my map',
-        x: 420, // depends on align
-        y: 60, // depends on font size
-        align: 'center',
-        font: {
-          name: 'times',
-          style: 'bold',
-          size: 20,
-        },
-      },
-    },
-    {
-      id: 'date',
-      rectangle: {
-        x: 840,
-        y: 20,
-        width: 263,
-        height: 60,
-      },
-      text: {
-        content: `Print date : ${new Date().toISOString().substr(0, 10)}`,
-        x: 850,
-        y: 50,
-      },
-    },
-    {
-      id: 'legend',
-      rectangle: {
-        x: 840,
-        y: 100,
-        width: 263,
-        height: 500,
-      },
-    },
-    {
-      id: 'projection',
-      rectangle: {
-        x: 840,
-        y: 620,
-        width: 263,
-        height: 74,
-      },
-    },
-    {
-      id: 'description',
-      rectangle: {
-        x: 20,
-        y: 620,
-        width: 800,
-        height: 74,
-      },
-    },
-    {
-      id: 'footer',
-      rectangle: {
-        x: 20,
-        y: 714,
-        width: 1083,
-        height: 60,
-      },
-    },
-  ],
-};
